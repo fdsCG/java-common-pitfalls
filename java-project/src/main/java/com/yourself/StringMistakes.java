88904a4c50da9f8a2eb7df0633ee83f5a845c1fc
fds <frederic@codingame.com>
2017-07-06 23:25:50 +0200

    update

---
+++ java-project/src/main/java/com/yourself/StringMistakes.java
@@ -0,0 +1,17 @@
+// { autofold
+package com.yourself;
+
+import java.util.Arrays;
+
+public class StringMistakes {
+
+
+public static void immutable() {
+// }
+String s = "Hello World!";
+s.toUpperCase();
+System.out.println(s);
+//{ autofold
+}
+}
+//}
---
+++ java-project/src/test/java/com/yourself/StringTests.java
@@ -0,0 +1,16 @@
+package com.yourself;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringTests {
+
+	@Test
+	public void Immutable() {
+		new StringsMistakes().immutable();
+	}
+}
