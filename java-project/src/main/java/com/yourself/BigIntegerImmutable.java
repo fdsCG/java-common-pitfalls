c9a8fb326fff3c58cb252e8ac4e80f711e8ced6b
fds <frederic@codingame.com>
2017-07-10 16:49:17 +0200

    update

--- java-project/src/main/java/com/yourself/BigIntegerImmutable.java
+++ java-project/src/main/java/com/yourself/BigIntegerImmutable.java
@@ -8,10 +8,12 @@ public class BigIntegerImmutable {
 
 public static void exec() {
 // }
+
 BigInteger b = new BigInteger("1000");
 b.add(new BigInteger("2"));
 
 System.out.println(b); // What does it print?
+
 //{ autofold
 }
 }
--- java-project/src/main/java/com/yourself/StringImmutable.java
+++ java-project/src/main/java/com/yourself/StringImmutable.java
@@ -6,10 +6,12 @@ public class StringImmutable {
 
 public static void exec() {
 // }
+
 String s = "abc";
 s.toUpperCase();
 
 System.out.println(s); // What does it print?
+
 //{ autofold
 }
 }
