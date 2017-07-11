23997c695041b40a076009043914ed3473891e4c
fds <frederic@codingame.com>
2017-07-11 18:48:25 +0200

    update

--- java-project/src/main/java/com/yourself/StringEquality.java
+++ java-project/src/main/java/com/yourself/StringEquality.java
@@ -9,12 +9,9 @@ public static void exec() {
 
 String s1 = "text";
 String s2 = "text";
-System.out.println(s1 == s2); // true or false?
-
 String s3 = "t" + "ext";
-String s4 = "tex" + "t";
-System.out.println(s3 == s4); // true or false?
 
+System.out.println(s1 == s2); // true or false?
 System.out.println(s2 == s3); // true or false?
 
 //{ autofold
